0060598c5e3721ecf422efd2b9fe44bfd984b829
TheoBerlin <theo.holmqvist@gmail.com>
2019-03-27 11:51:58 +0100

    Enable memory leak detection

--- GameProject/main.cpp
+++ GameProject/main.cpp
@@ -1,7 +1,11 @@
 #include <Engine/Utils/Logger.hpp>
+#include <crtdbg.h>
 
 int main()
 {
+	// Check for memory leaks. Disabled when not debugging
+	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF);
+
 	Logger::init();
 
 	return 0;
--- GameProject/main_test.cpp
+++ GameProject/main_test.cpp
@@ -1,9 +1,14 @@
 #define CATCH_CONFIG_RUNNER
 
+#include <crtdbg.h>
 #include <catch/catch.hpp>
 
 int main(int argc, char* argv[])
 {
+	// Global setup
+	// Check for memory leaks. Disabled when not debugging
+	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF);
+
     // Run tests
 	int result = Catch::Session().run(argc, argv);
 
