26f4839c8910dcd2814728c5fe9e4088891dd6dd
TheoBerlin <theo.holmqvist@gmail.com>
2020-04-27 14:39:00 +0200

    Doppler effect

--- GameProject/Engine/Audio/SoundPlayer.hpp
+++ GameProject/Engine/Audio/SoundPlayer.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#define NOMINMAX
+
 #include <Engine/ECS/System.hpp>
 #include <Engine/Utils/IDVector.hpp>
+
+#include <DirectXMath.h>
 
 class LightHandler;
 class SoundHandler;
 class TransformHandler;
+class VelocityHandler;
+struct Sound;
 
 class SoundPlayer : public System
 {
@@ -18,10 +24,15 @@ public:
     virtual void update(float dt) override;
 
 private:
+    void stereoPan(Sound& sound, const DirectX::XMVECTOR& camToSound, const DirectX::XMVECTOR& camDirFlat);
+    void dopplerEffect(Sound& sound, const DirectX::XMVECTOR& camPos, const DirectX::XMVECTOR& camVelocity, float camSpeed, const DirectX::XMVECTOR& objectPos, const DirectX::XMFLOAT3& objectVelocity);
+
+private:
     IDVector m_Sounds;
     IDVector m_Cameras;
 
     LightHandler* m_pLightHandler;
     SoundHandler* m_pSoundHandler;
     TransformHandler* m_pTransformHandler;
+    VelocityHandler* m_pVelocityHandler;
 };
