e0e186baefe9a619d58a28a3e590be4a45e6e8f5
TheoBerlin <theo.holmqvist@gmail.com>
2020-08-26 22:41:37 +0200

    Use gitpython for git commands

--- .github/workflows/update-charts.py
+++ .github/workflows/update-charts.py
@@ -1,7 +1,9 @@
 import json, os, subprocess, sys, getopt
+from git import Repo
 
 repoOwner   = 'TheoBerlin'
-repo        = 'SoloGame'
+repoName    = 'SoloGamePages'
+repoDir     = 'docs'
 
 benchmarkFileName = 'benchmark_results.json'
 
@@ -10,8 +12,12 @@ def print_help(helpString, args):
     print(helpString)
     print(f'Used flags: {str(args)}')
 
+def pull_pages_repo(pat_token):
+    repoURL = f'https://{pat_token}:x-oauth-basic@github.com/{repoOwner}/{repoName}'
+    return Repo.clone_from(repoURL, repoDir)
+
 def get_commit_info(commitID):
-    URL = f'https://api.github.com/repos/{repoOwner}/{repo}/commits/{commitID}'
+    URL = f'https://api.github.com/repos/{repoOwner}/{repoName}/commits/{commitID}'
 
     import requests
     resp = requests.get(URL)
@@ -30,7 +36,7 @@ def update_average_fps_chart(chartData, vkResults, dx11Results, commitID):
     chartData['labels'].append(commitID[:7])
     chartData['tooltips'].append(f'{commitMsg}\n{timestamp}')
 
-def update_charts(commitID, dataPath, vkResultsPath, dx11ResultsPath):
+def update_charts(commitID, vkResultsPath, dx11ResultsPath):
     print('Updating charts in docs/')
 
     with open(vkResultsPath, 'r') as benchmarkFile:
@@ -41,7 +47,7 @@ def update_charts(commitID, dataPath, vkResultsPath, dx11ResultsPath):
         dx11Results = json.load(benchmarkFile)
         benchmarkFile.close()
 
-    with open(f'{dataPath}/charts.json', 'r+') as chartsFile:
+    with open('_data/charts.json', 'r+') as chartsFile:
         chartsData = json.load(chartsFile)
         update_average_fps_chart(chartsData['AverageFPS'], vkResults, dx11Results, commitID)
         chartsFile.seek(0)
@@ -49,23 +55,11 @@ def update_charts(commitID, dataPath, vkResultsPath, dx11ResultsPath):
         chartsFile.truncate()
         chartsFile.close()
 
-def run_git_command(args):
-    completedProcess = subprocess.run(args, capture_output=True)
-    if completedProcess.returncode != 0:
-        print(f"Failed:\n{completedProcess.stderr}")
-        return 1
-
-    print(" Success")
-    return 0
-
-def commit_changes(dataPath):
-    args = ['git', 'add', f'{dataPath}/*']
-
-    run_git_command(args)
-    args = ['git', 'commit', '-m', '\"Update charts data\"']
-
-    run_git_command(args)
-    args = ['git', 'push', '-u', 'origin', 'master']
+def commit_changes(pagesRepo):
+    pagesRepo.index.git.add(['_data/*'], update=True)
+    pagesRepo.index.commit('-m', 'Update charts data')
+    origin = pagesRepo.remote(name='origin')
+    origin.push()
 
 def main(argv):
     commitID = '8ed033477cd8e94bd9d10bfccc67efaf3b4db3f5'
@@ -75,17 +69,15 @@ def main(argv):
         sys.exit(1)
 
     helpStr = '''usage: --data <path_to_pages_data> --vk <path_to_vk_results.json> --dx11 <path_to_dx11_results.json> --pat <PAT_TOKEN>\n
-        data: relative path to _data directory inside the pages repo\n
         vk: path to .JSON file to retrieve Vulkan benchmarks results from\n
-        dx11: path to .JSON file to retrieve DirectX 11 benchmarks results from
+        dx11: path to .JSON file to retrieve DirectX 11 benchmarks results from\n
         pat: Personal Access Token with rights to write to the GitHub Pages repo'''
     try:
-        opts, args = getopt.getopt(argv, 'h', ['help', 'data=', 'vk=', 'dx11=', 'pat='])
+        opts, args = getopt.getopt(argv, 'h', ['help', 'vk=', 'dx11=', 'pat='])
     except getopt.GetoptError:
         print_help(helpStr, args)
         sys.exit(1)
 
-    dataPath        = None
     vkResultsPath   = None
     dx11ResultsPath = None
     pat_token       = None
@@ -94,8 +86,6 @@ def main(argv):
         if opt in ['-h', '--help']:
             print_help(helpStr, args)
             sys.exit(1)
-        if opt == '--data':
-            dataPath = arg
         elif opt == '--vk':
             vkResultsPath = arg
         elif opt == '--dx11':
@@ -103,16 +93,15 @@ def main(argv):
         elif opt == '--pat':
             pat_token = arg
 
-    if not dataPath or not vkResultsPath or not dx11ResultsPath or not pat_token:
+    if not vkResultsPath or not dx11ResultsPath or not pat_token:
         print('Missing argument(s)')
         print_help(helpStr, args)
         sys.exit(1)
 
-    if dataPath.endswith('/'):
-        dataPath = dataPath[:-1]
-
-    update_charts(commitID, dataPath, vkResultsPath, dx11ResultsPath)
-    commit_changes(dataPath)
+    pagesRepo = pull_pages_repo(pat_token)
+    os.chdir(repoDir)
+    update_charts(commitID, f'../{vkResultsPath}', f'../{dx11ResultsPath}')
+    commit_changes(pagesRepo)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
